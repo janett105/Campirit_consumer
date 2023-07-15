87cdcf7b6644a84e24de3eef408890ea51021cb8
jihoo <janett1005@naver.com>
2023-07-15 14:46:13 +0900

    Update MainPage.js

--- FRONTEND/screens/MainPage.js
+++ FRONTEND/screens/MainPage.js
@@ -9,185 +9,293 @@ import {
   Platform,
   StatusBar,
   Animated,
-  Alert,
+  ScrollView,
   ImageBackground,} from 'react-native';
+import AppLoading from 'expo-app-loading';
 import { useFonts } from 'expo-font';
 import { getStatusBarHeight } from 'react-native-status-bar-height';
+import ShowItem from '../components/ShowItem.js';
 
 const windowWidth = Dimensions.get('window').width;
 const windowHeight = Dimensions.get('window').height;
 const StatusBarHeight =
     Platform.OS === 'ios' ? getStatusBarHeight(true) : StatusBar.currentHeight;
 
-export default function MainPage({ navigation }) {
-    const [kit, setKit] = React.useState({
-        텐트 : [
-            {
-                name: '짱 이쁜 텐트래',
-                size: 's',
-                color: '검정',
-                //사용자 선택 사항들
-            },
-            {
-                name: '',
-                size: 'm',
-                color: '노란색',
-                //사용자 선택 사항들
-            }
-        ],
-        타프: [
-            {
-                name: '타프1',
-                size: 's',
-                color: '검정',
-                //사용자 선택 사항들
-            },
-            {
-                name: '타프2',
-                size: 'm',
-                color: '노란색',
-            //사용자 선택 사항들
-            }
-        ],
-        //키트 상품들
-    });
 
-    
-    const NEXT =()=>{
-      // navigation.navigate('')
-    }
+export default props => {
+  const NEXT =()=>{
+    setNow(flow)
+    navigation.navigate('Makekit');
+  }
+  const[order, setOrder] = useState(
+    ["텐트", "타프","매트/침낭", "테이블", "체어",  "주방 용품", "난로", "에어컨", "기타용품", "감성 용품","폴딩 박스"]
+  );
+  const[now, setNow] = useState(0);
+  const[flow, setFlow] = useState(0);
+  const [kit, setKit] = useState({
+    텐트 : [
+      {
+        name: '텐트 A',
+        size: '',
+        color: '',
+        price:5000,
+         //사용자 선택 사항들
+      },
+      {
+        name: '바람막이 텐트 어쩌고 저쩌고',
+        size: '',
+        color: '',
+        price:500000,
+         //사용자 선택 사항들
+      },
+      {
+        name: '텐트 C',
+        size: '',
+        color: '',
+        price:1000,
+         //사용자 선택 사항들
+      },
+    ],
+    타프: [
+      {
+        name: '타프A',
+        size: '',
+        color: '',
+        price:5000,
+         //사용자 선택 사항들
+      },
+      {
+        name: '타프B일까?',
+        size: '',
+        color: '',
+        price:5000,
+         //사용자 선택 사항들
+      },
+    ],
+    매트침낭:[],
+    테이블:[],
+    체어:[],
+    주방용품:[],
+    난로:[],
+    에어컨:[],
+    기타용품:[],
+    감성용품:[],
+    폴딩박스:[]
+});
 
-    const [loaded] = useFonts({
-      YiSunShin_B: require('../assets/fonts/이순신돋움체B.ttf'),
-      Roboto:require('../assets/fonts/Roboto.ttf'),
-    });
-    if (!loaded) {
-      return null;
-    }
+  let [fontsLoaded] = useFonts({
+    'YiSunShin_B': require('../assets/fonts/이순신돋움체B.ttf'),
+  });
 
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  } else {
     return (
       <View style={styles.container}>
-{/* ///////////////////////////////<<<< Header : 되돌아가기 버튼 >>>>>////////////////////////////////////// */}
-        <View style={styles.header}>
-          <View style={styles.BackToTent}>
-            <Text>텐트</Text>
+        <ScrollView contentContainerStyle={{flexGrow: 1}} >
+          <View style={{height:windowHeight/12}}>
+            <ScrollView
+            nestedScrollEnabled={true}
+            horizontal = {true}
+            showsHorizontalScrollIndicator= {false}>
+              <Pressable 
+                onPress={()=>{navigation.navigate('MakeKit'), setNow(0)}}
+                style={styles.btn_backto}>
+                <Image source={require("../assets/images/MainPage/MainTent.png")}/>
+                <Text style={styles.btn_backText}>텐트</Text>
+              </Pressable>
+              <Pressable 
+                onPress={()=>{navigation.navigate('MakeKit'), setNow(1)}}
+                style={styles.btn_backto}>
+                <Image source={require("../assets/images/MainPage/MainTaff.png")}/>
+                <Text style={styles.btn_backText}>타프</Text>
+              </Pressable>
+              <Pressable 
+                onPress={()=>{navigation.navigate('MakeKit', setNow(2))}}
+                style={styles.btn_backto}>
+                <Image source={require("../assets/images/MainPage/MainMat.png")}/>
+                <Text style={styles.btn_backText}>매트/침낭</Text>
+              </Pressable>
+              <Pressable 
+                onPress={()=>{navigation.navigate('MakeKit'), setNow(3)}}
+                style={styles.btn_backto}>
+                <Image source={require("../assets/images/MainPage/MainTable.png")}/>
+                <Text style={styles.btn_backText}>테이블</Text>
+              </Pressable>
+              <Pressable 
+                onPress={()=>{navigation.navigate('MakeKit'), setNow(4)}}
+                style={styles.btn_backto}>
+                <Image source={require("../assets/images/MainPage/MainMat.png")}/>
+                <Text style={styles.btn_backText}>체어</Text>
+              </Pressable>
+              <Pressable 
+                onPress={()=>{navigation.navigate('MakeKit'), setNow(5)}}
+                style={styles.btn_backto}>
+                <Image source={require("../assets/images/MainPage/MainKU.png")}/>
+                <Text style={styles.btn_backText}>주방 용품</Text>
+              </Pressable>
+              <Pressable 
+                onPress={()=>{navigation.navigate('MakeKit'), setNow(6)}}
+                style={styles.btn_backto}>
+                <Image source={require("../assets/images/MainPage/MainMat.png")}/>
+                <Text style={styles.btn_backText}>난로</Text>
+              </Pressable>
+              <Pressable 
+                onPress={()=>{navigation.navigate('MakeKit'), setNow(7)}}
+                style={styles.btn_backto}>
+                <Image source={require("../assets/images/MainPage/MainAC.png")}/>
+                <Text style={styles.btn_backText}>에어컨</Text>
+              </Pressable>
+              <Pressable 
+                onPress={()=>{navigation.navigate('MakeKit'), setNow(8)}}
+                style={styles.btn_backto}>
+                <Image source={require("../assets/images/MainPage/MainMat.png")}/>
+                <Text style={styles.btn_backText}>기타 용품</Text>
+              </Pressable>
+              <Pressable 
+                onPress={()=>{navigation.navigate('MakeKit'), setNow(10)}}
+                style={styles.btn_backto}>
+                <Image source={require("../assets/images/MainPage/MainGamsung.png")}/>
+                <Text style={styles.btn_backText}>감성 용품</Text>
+              </Pressable>
+              <Pressable 
+                onPress={()=>{navigation.navigate('MakeKit'), setNow(11)}}
+                style={styles.btn_backto}>
+                <Image source={require("../assets/images/MainPage/MainBox.png")}/>
+                <Text style={styles.btn_backText}>폴딩 박스</Text>
+              </Pressable>
+            </ScrollView>
           </View>
-          <View style={styles.BackToTaff}>
-            <Text>타프</Text>
+          <View style={styles.body} minHeight={windowHeight/1.17}>
+            <ImageBackground 
+              style={styles.kitImage}
+              source={require("../assets/images/MainPage/kitBackground.png")}>
+            </ImageBackground>
+            <View style={styles.kitlstTitle}>
+              <Text style={styles.titleText}>나만의 스타터 키트 구성품</Text>
+            </View>
+            <View style={styles.kitLst}>
+                <ShowItem ItemList={kit.텐트}/>
+                <ShowItem ItemList={kit.타프}/>
+                <ShowItem ItemList={kit.매트침낭}/>
+                <ShowItem ItemList={kit.테이블}/>
+                <ShowItem ItemList={kit.체어}/>
+                <ShowItem ItemList={kit.주방용품}/>
+                <ShowItem ItemList={kit.난로}/>
+                <ShowItem ItemList={kit.에어컨}/>
+                <ShowItem ItemList={kit.기타용품}/>
+                <ShowItem ItemList={kit.감성용품}/>
+                <ShowItem ItemList={kit.폴딩박스}/>
+            </View>
           </View>
-        </View>
-{/* ///////////////////////////////<<<< Body : 현재 선택 물품들 by 그림, 상세 물품 >>>>>////////////////////////////////////// */}
-        <ImageBackground 
-            style={styles.kitImage}
-            source={require("../assets/images/MainPage/kitBackground.png")}>
-        </ImageBackground>
-        <View style={styles.kitlstTitle}>
-          <Text style={styles.titleText}>나만의 스타터 키트 구성품</Text>
-        </View>
-        <View style={styles.kitLst}>
-          <Text>여기에 물품 목록들</Text>
-        </View>
-{/* ///////////////////////////////<<<< Footer : 자동차, 다음 버튼 >>>>>////////////////////////////////////// */}
+        </ScrollView>
         <View style={styles.footer}>
           <View style = {styles.process}>
             <Animated.Image
-                source={require("../assets/images/Quiz/movingCar.png")}
-                style={styles.movingCar}/>
+              source={require("../assets/images/Quiz/movingCar.png")}
+              style={styles.movingCar}/>
             <View style = {styles.bundle}>
-              <Pressable style={styles.btn_Next} onPress={NEXT}>
-                <Text style={styles.btn_nextText}>다음</Text>
-             </Pressable>
-              <Text style = {styles.processText}>45%</Text>
+            <Pressable style={styles.btn_Next} onPress={NEXT}>
+              <Text style={styles.btn_nextText}>다음</Text>
+            </Pressable>
+            <Text style = {styles.processText}>45%</Text>
             </View>
           </View>
-            <Image
-              source={require("../assets/images/Quiz/Line.png")}/>
+          <Image
+            source={require("../assets/images/Quiz/Line.png")}/>
         </View>
         <StatusBar backgroundColor='#EEECE0'></StatusBar>
-      </View>
+      </View>  
     );
-}
+  }
+};
 
 const styles = StyleSheet.create({
-  container:{
-    flex : 1,
-    backgroundColor:"white",
-  },
-/* ///////////////////////////////<<<< Header : 되돌아가기 버튼 >>>>>////////////////////////////////////// */
-  header:{
-    flex:0.14,
-    marginTop:StatusBarHeight,
+  container: {
     backgroundColor:"#EEECE0",
-
-    flexDirection:"row",
-    justifyContent:"space-around", 
+    flex: 1,
+    
+    marginTop:StatusBarHeight,
   },
-  BackToTent:{
-    backgroundColor:"pink",
+  btn_backto:{
+    // backgroundColor:"pink",
+    width:windowWidth/7,
+  
+    marginHorizontal:windowWidth/50,
+  
+    alignItems:'center',
+    justifyContent:'center'
   },
-  BackToTaff:{
-    backgroundColor:"tomato",
+  btn_backText:{
+    fontWeight:"400",
+    fontSize:12,
+    fontFamily:'YiSunShin_B',
+  
+    marginTop:6,
+  },  
+  body:{
+    backgroundColor:"green", 
   },
-/* ///////////////////////////////<<<< Body : 현재 선택 물품들 by 그림, 상세 물품 >>>>>////////////////////////////////////// */
   kitImage:{
-    width: windowWidth,
-    height: 355,
+   height:windowHeight/2.2,
+   width:windowWidth,
   },
   kitlstTitle:{
-    flex:0.11,
-    justifyContent:"center",
+    backgroundColor:"white",
+    height:windowHeight/22,
+  
+    justifyContent:'center',
   },
   titleText:{
     fontWeight:"400",
     fontSize:14,
     fontFamily:'YiSunShin_B',
-
+  
     marginLeft:10,
   },
   kitLst:{
-    flex:0.89,
-    backgroundColor:"tomato",
-    height: windowHeight/23,
+    backgroundColor:"yellow",
+    flex:1,
   },
-/* ///////////////////////////////<<<< Footer : 진도율 표시 >>>>>////////////////////////////////////// */
-footer:{
-  flex: 0.17,
-  backgroundColor:"white",
-},
-process:{
-  flexDirection:"row",
-  justifyContent:"flex-end",
-},
-movingCar:{
-  alignSelf:"flex-end",
-  marginBottom:-2,
-  marginRight: windowWidth-300,
-},
-btn_Next:{
-  backgroundColor: "#F2C892",
-  borderRadius:9,
-  width:65,
-  height:25,
-
-  marginRight:7,
-
-  justifyContent:"center",
-  alignItems:"center",
-},
-btn_nextText:{
-  fontWeight:"400",
-  fontSize:14,
-  fontFamily:'YiSunShin_B',
-},
-processText:{
-  alignSelf:"flex-end",
-  marginTop:2,
-  marginBottom:3,
-  marginRight:5,
-
-  color:"#497860",
-  fontWeight:"400",
-  fontSize:12,
-  fontFamily:'YiSunShin_B',
-},
-});
+  footer:{
+    backgroundColor:"white",
+    height:windowHeight/15,
+  },
+  process:{
+    flexDirection:"row",
+    justifyContent:"flex-end",
+  },
+  movingCar:{
+    alignSelf:"flex-end",
+    marginBottom:-9,
+    marginRight: windowWidth-300,
+  },
+  btn_Next:{
+    backgroundColor: "#F2C892",
+    borderRadius:9,
+    width:65,
+    height:25,
+  
+    marginRight:7,
+  
+    justifyContent:"center",
+    alignItems:"center",
+  },
+  btn_nextText:{
+    fontWeight:"400",
+    fontSize:14,
+    fontFamily:'YiSunShin_B',
+  },
+  processText:{
+    alignSelf:"flex-end",
+    marginTop:2,
+    marginBottom:3,
+    marginRight:5,
+  
+    color:"#497860",
+    fontWeight:"400",
+    fontSize:12,
+    fontFamily:'YiSunShin_B',
+  },
+  });
+  
